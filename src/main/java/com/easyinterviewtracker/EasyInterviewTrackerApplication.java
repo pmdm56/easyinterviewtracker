f04010aea7feee113f85734af21d70c0801db414
Pedro <pedromoreira56@hotmail.com>
2025-02-11 13:16:56 +0000

    configuration files

---
+++ src/main/java/com/easyinterviewtracker/EasyInterviewTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.easyinterviewtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyInterviewTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyInterviewTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyinterviewtracker/EasyInterviewTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyinterviewtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyInterviewTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

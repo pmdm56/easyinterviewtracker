0eafc36348ed0495406b4c5159e98b94f0c6be34
Pedro <pedromoreira56@hotmail.com>
2025-02-11 13:17:08 +0000

    implements User

---
+++ src/main/java/com/easyinterviewtracker/domain/User.java
@@ -0,0 +1,30 @@
+package com.easyinterviewtracker.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<JobApplication> jobApplications;
+
+}
